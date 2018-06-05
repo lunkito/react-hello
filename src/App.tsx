8ffd2772ca788e5128e29c451f4be56dfccfd80b
Pablo <pablo.martingu@gmail.com>
2018-06-05 21:03:27 +0100

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,38 @@ import Movies from './pages/Movies';
 import newMovieControlled from './pages/NewMovieControlled';
 import newMovieUncontrolled from './pages/NewMovieUncontrolled';
 
+// REDUX ---------------------------------------------
+import * as Redux from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer, { AppState } from './reducers';
+import { decrementCounter, incrementCounter } from './actions';
+
+const store: Redux.Store<AppState> = Redux.createStore(rootReducer);
+
+function increment() {
+  store.dispatch(incrementCounter(1));
+}
+
+function decrement() {
+  store.dispatch(decrementCounter(1));
+}
+
 export default class App extends React.Component {
   render() {
-    return <Router>
-      <div>
-        <Header/>
-        <Route exact path='/' component={Home}/>
-        <Route path='/home' component={Home}/>
-        <Route path='/about' component={About}/>
-        <Route path='/movies' component={Movies}/>
-        <Route path='/newMovieControlled' component={newMovieControlled}/>
-        <Route path='/newMovieUncontrolled' component={newMovieUncontrolled}/>
-      </div>
-    </Router>;
+    return <Provider store={store}>
+      <Router>
+        <div>
+          <Header/>
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <Route exact path='/' component={Home}/>
+          <Route path='/home' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/movies' component={Movies}/>
+          <Route path='/newMovieControlled' component={newMovieControlled}/>
+          <Route path='/newMovieUncontrolled' component={newMovieUncontrolled}/>
+        </div>
+      </Router>
+    </Provider>;
   }
 }
--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,9 +1,44 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { AppState } from '../reducers';
+import { incrementCounter } from '../actions';
+import { connect } from 'react-redux';
 
-export default class Header extends React.Component {
+export interface HeaderProps {
+  counter: number;
+  incrementCounter?: (value: number) => {};
+  decrementCounter?: (value: number) => {};
+}
+
+const mapStateToProps = (state: AppState) => {
+  return { counter: state.counter.value };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  incrementCounter: (value: number) => dispatch(incrementCounter(value)),
+  decrementCounter: (value: number) => dispatch(incrementCounter(value))
+});
+
+class Header extends React.Component<HeaderProps, {}> {
+  constructor(props) {
+    super(props);
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.props.incrementCounter(2);
+  }
+  decrement() {
+    this.props.decrementCounter(2);
+  }
+
   render() {
     return <ul>
+      <li>Number of movies: {this.props.counter}</li>
+      <li><button onClick={this.increment}>+</button></li>
+      <li><button onClick={this.decrement}>-</button></li>
       <li><Link to='/home'>Home</Link></li>
       <li><Link to='/about'>About</Link></li>
       <li><Link to='/movies'>Movies</Link></li>
@@ -12,3 +47,5 @@ export default class Header extends React.Component {
     </ul>;
   }
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
